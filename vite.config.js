054d8d665e0a373b2ef47076f4c541f79a5d0740
sivaneshkkf <sivaneshkkf@gmail.com>
2024-11-19 11:14:35 +0300

    fix: logo

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,8 @@ export default defineConfig({
       manifest: {
         name: "Sivanesh Portfolio",
         short_name: "Portfolio",
-        description: "A sleek and modern portfolio website showcasing skills and projects built with React JS.",
+        description:
+          "A sleek and modern portfolio website showcasing skills and projects built with React JS.",
         theme_color: "#1a1a18",
         background_color: "#1a1a18",
         display: "standalone",
@@ -21,29 +22,41 @@ export default defineConfig({
         base: "/Portfolio/",
         icons: [
           {
-            src: "android-chrome-192x192.png",
+            purpose: "maskable",
             sizes: "192x192",
+            src: "android-chrome-192x192.png",
             type: "image/png",
           },
           {
-            src: "android-chrome-512x512.png",
+            purpose: "any",
+            sizes: "144x144",
+            src: "android-chrome-144x144.png",
+            type: "image/png",
+          },
+          {
+            purpose: "any",
             sizes: "512x512",
+            src: "android-chrome-512x512.png",
             type: "image/png",
           },
+        ],
+        screenshots: [
           {
-            src: "apple-touch-icon.png",
-            sizes: "180x180",
+            src: "Capture.webp",
+            sizes: "1920x1080",
             type: "image/png",
+            form_factor: "wide",
           },
           {
-            src: "favicon-32x32.png",
-            sizes: "32x32",
+            src: "Capture1.webp",
+            sizes: "720x1280",
             type: "image/png",
+            form_factor: "narrow",
           },
         ],
       },
     }),
   ],
-  base: '/Portfolio/',
-  assetsInclude: ['**/*.lottie'],
-})
+  base: "/Portfolio/",
+  assetsInclude: ["**/*.lottie"],
+});
